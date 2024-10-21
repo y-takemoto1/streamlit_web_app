f996633a8193910bedec27cc2cc1f3081630462e
takemoto <y-takemoto@kts-it.com>
2024-10-21 18:04:23 +0900

    test

--- mynavi-ms.py
+++ mynavi-ms.py
@@ -9,7 +9,7 @@ import os
 st.text('ルート確認１')
 
 # スクレイピングしたいURL（例: 福岡の求人）
-url = 'https://mynavi-ms.jp/search/fukuoka/area-all'
+url = 'https://www.staff-q.co.jp/recruiter'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
