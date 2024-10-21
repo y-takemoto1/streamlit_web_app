e917ab94ef91fa781b2067a538d143021bd5c86b
takemoto <y-takemoto@kts-it.com>
2024-10-21 17:19:19 +0900

    test

--- mynavi-ms.py
+++ mynavi-ms.py
@@ -12,7 +12,7 @@ st.text('ルート確認１')
 url = 'https://mynavi-ms.jp/search/fukuoka/area-all'
 
 headers = {
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'
         }
 
 # エクセルを開く
